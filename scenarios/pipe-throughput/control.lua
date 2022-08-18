0a963947efa70de926b68bacefb621cdf605c22d
Bilka <Bilka2@users.noreply.github.com>
2022-08-18 17:57:16 +0200

    South flow direction
    No changes

--- scenarios/pipe-throughput/control.lua
+++ scenarios/pipe-throughput/control.lua
@@ -2,7 +2,8 @@
 ---@param surface LuaSurface
 ---@param offset int
 local function spawn_row(length, surface, offset)
-  local row_height = global.row_index * 3
+  -- rows are north -> south and height is horizontal. basically it should be columns and width
+  local row_height = global.row_index * 3 
   local pump_size = 2/2
 
   ---@class Row
@@ -13,21 +14,21 @@ local function spawn_row(length, surface, offset)
   global.rows[global.row_index] = {}
   local row = global.rows[global.row_index]
   row.length = length
-  row.source = surface.create_entity{name="storage-tank", position = { x = offset + length + pump_size + 2, y = row_height + offset + 1}, force = "player"}
-  row.sink = surface.create_entity{name="storage-tank", position = { x = offset - 3 - pump_size, y = row_height + offset - 1}, force = "player"}
+  row.source = surface.create_entity{name="storage-tank", position = {  x = row_height + offset - 1, y = offset - 3 - pump_size}, force = "player"}
+  row.sink = surface.create_entity{name="storage-tank", position = { x = row_height + offset + 1, y = offset + length + pump_size + 2}, force = "player"}
 
   row.pumps = {}
-  row.pumps[2] = surface.create_entity{name="pump", position = { x = offset + length + pump_size, y = row_height + offset}, direction = defines.direction.west, force = "player"}
-  surface.create_entity{name="substation", position = { x = offset + length + pump_size + 5, y = row_height + offset}, force = "player"}
-  row.pumps[1] = surface.create_entity{name="pump", position = { x = offset - pump_size, y = row_height + offset}, direction = defines.direction.west, force = "player"}
-  surface.create_entity{name="substation", position = { x = offset - 5 - pump_size, y = row_height + offset}, force = "player"}
+  row.pumps[2] = surface.create_entity{name="pump", position = { x = row_height + offset, y = offset + length + pump_size}, direction = defines.direction.south, force = "player"}
+  surface.create_entity{name="substation", position = { x = row_height + offset, y = offset + length + pump_size + 5}, force = "player"}
+  row.pumps[1] = surface.create_entity{name="pump", position = { x = row_height + offset, y = offset - pump_size}, direction = defines.direction.south, force = "player"}
+  surface.create_entity{name="substation", position = { x = row_height + offset, y = offset - 5 - pump_size}, force = "player"}
   for _, pump in pairs(row.pumps) do
     rendering.draw_text{text=tostring(length), surface = surface, color = {1,1,1}, scale = 2, target = pump}
   end
 
   assert(length >= 1)
   for i=0,length-1 do
-    surface.create_entity{name="pipe", position = { x = offset + i, y = row_height + offset}, force = "player"}
+    surface.create_entity{name="pipe", position = { x = row_height + offset, y = offset + i}, force = "player"}
   end
 
   global.row_index = global.row_index + 1
@@ -50,7 +51,7 @@ script.on_event(defines.events.on_player_created, function(event)
   --spawn power
   --note, if not starting with pipe length 1, the pumps on the right (near "sink" storage tanks) may not be powered
   local offset = -1450
-  local eei = surface.create_entity{name="electric-energy-interface", position={x = offset - 10, y = offset - 4}, force = "player"}
+  local eei = surface.create_entity{name="electric-energy-interface", position={x = offset - 4, y = offset - 10}, force = "player"}
   eei.electric_buffer_size = 1000000000
   eei.power_production = 1000000000
 
@@ -131,7 +132,7 @@ script.on_nth_tick(10, function(event)
     game.tick_paused = true
     game.speed = 1
 
-    if true then -- set to false when using a factorio version where you can read pump speed with mods
+    if false then -- set to false when using a factorio version where you can read pump speed with mods
       game.print("Pumping speed values should be balanced out (verified with pipe lengths up to 1001), you can now read them from the tooltips")
     else -- custom factorio version that can read pump speed with mods
       game.print("Pumping speed values should be balanced out. If not, pipe lengths with problem are printed here in chat")
